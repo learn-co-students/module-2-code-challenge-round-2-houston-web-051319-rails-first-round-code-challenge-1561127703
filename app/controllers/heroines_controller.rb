257a565b5e29deacffa437be739fad99d842f4e5
Hans Krohn <hanskrohn70@gmail.com>
2019-06-21 10:09:22 -0500

    Finished

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,25 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+  def show
+    @hero = Heroine.find(params[:id])
+  end
+  def new
+    @powers=Power.all
+  end
+  def create
+    byebug
+    x = Heroine.create(hero_params[:hero])
+    byebug
+    redirect_to "/heroines/#{x.id}"
+  end
+  def hero_params
+    params.permit(
+      hero: [
+        :name,
+        :super_name,
+        :power_id
+      ]
+    )
+    end
 end
